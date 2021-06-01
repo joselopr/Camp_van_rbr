c2053c435dbcf8543d5f25b0062ac833dee189d7
jose prieto <josel0priet0@gmail.com>
2021-06-01 16:19:06 +0200

    some code added to show#action, also made some modification to the seed

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,31 +1,34 @@
 class VansController < ApplicationController
-
+  before_action :set_van, only: [:show]
   def index
     @vans = policy_scope(Van)
   end
 
   def show
-
   end
 
-  def new
+  # def new
 
+  # end
+
+  # def create
+
+  # end
+
+  # def edit
+
+  # end
+
+  # def update
+
+  # end
+
+  # def destroy
+
+  # end
+  private
+
+  def set_van
+    @van = Van.find(params[:id])
   end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
-
 end
