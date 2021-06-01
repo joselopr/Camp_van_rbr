6bac05c7eef6eaf9de0da2d672acfd2f3aba1e08
Daniel Mendez <danielsaso@gmail.com>
2021-06-01 12:26:29 +0200

    Fixed the seed, added the faker Gem and created the van controller with index view

---
+++ app/controllers/vans_controller.rb
@@ -0,0 +1,31 @@
+class VansController < ApplicationController
+
+  def index
+    @vans = Van.all
+  end
+
+  # def show
+
+  # end
+
+  # def new
+
+  # end
+
+  # def create
+
+  # end
+
+  # def edit
+
+  # end
+
+  # def update
+
+  # end
+
+  # def destroy
+
+  # end
+
+end
