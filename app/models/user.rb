976c3053e51db68569f0a7e8360220f9116828d5
Daniel Mendez <danielsaso@gmail.com>
2021-06-01 13:01:47 +0200

    working on model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :vans
+
+  validates :first_name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :last_name, presence: true, uniqueness: true, length: { minimum: 3 }
 end
--- app/models/van.rb
+++ app/models/van.rb
@@ -2,4 +2,8 @@ class Van < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings # <- for van reviews
+
+
+  validates :brand, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :model, presence: true, uniqueness: true, length: { minimum: 3 }
 end
