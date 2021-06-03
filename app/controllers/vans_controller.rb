8305279bcb8ef36e118eff00d212a643bbcb7b0e
OnnoGoudsmit <79285113+OnnoGoudsmit@users.noreply.github.com>
2021-06-03 15:45:25 +0200

    Update vans_controller.rb
    Everything was deleted, pasted original

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -2,6 +2,7 @@ class VansController < ApplicationController
   before_action :set_van, only: [:show]
   def index
     @vans = policy_scope(Van)
+
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @vans.geocoded.map do |van|
       {
@@ -14,29 +15,37 @@ class VansController < ApplicationController
   end
 
   def show
-    @booking = Booking.new
   end
 
-  # def new
+  def new
+    @van = Van.new
+    authorize @van
+  end
 
-  # end
-
-  # def create
-
-  # end
+  def create
+    @van = Van.new(van_params)
+    @van.user = current_user
+    authorize @van
+    if @van.save
+      redirect_to @van, notice: 'Van was successfully created.'
+    else
+      render :new
+    end
+  end
 
   # def edit
   # end
-
   # def update
   # end
-
   # def destroy
   # end
   private
-
   def set_van
     @van = Van.find(params[:id])
     authorize @van
+  end
+
+  def van_params
+    params.require(:van).permit(:brand, :model, :capacity, :day_price, :description)
   end
 end
