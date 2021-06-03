e091081c806747a24e52b7f3d549a6d89d116540
jose prieto <josel0priet0@gmail.com>
2021-06-03 10:41:55 +0200

    new branch created

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -2,34 +2,19 @@ class VansController < ApplicationController
   before_action :set_van, only: [:show]
   def index
     @vans = policy_scope(Van)
-
-    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    @markers = @vans.geocoded.map do |van|
-      {
-        lat: van.latitude,
-        lng: van.longitude
-      }
-    end
   end
 
   def show
+
   end
 
-  def new
-    @van = Van.new
-    authorize @van
-  end
+  # def new
 
-  def create
-    @van = Van.new(van_params)
-    @van.user = current_user
-    authorize @van
-    if @van.save
-      redirect_to @van, notice: 'Van was successfully created.'
-    else
-      render :new
-    end
-  end
+  # end
+
+  # def create
+
+  # end
 
   # def edit
   # end
@@ -44,9 +29,5 @@ class VansController < ApplicationController
   def set_van
     @van = Van.find(params[:id])
     authorize @van
-  end
-
-  def van_params
-    params.require(:van).permit(:brand, :model, :capacity, :day_price, :description)
   end
 end
